dadfe358124655a0d0c0fd8cdaecf0a6b37817d4
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2024-04-23 18:28:49 +0200

    archived

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import Index from './components/Index/Index'
-import reportWebVitals from './reportWebVitals'
 import './fonts/Poppins-Medium.ttf'
 import './fonts/Poppins-Regular.ttf'
 import './fonts/Poppins-Bold.ttf'
@@ -13,8 +12,3 @@ root.render(
         <Index />
     </React.StrictMode>
 )
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
